eb43291fe8fe2c51ca10ee4ba8725dd1692a7ee4
SamSvetlik <sam.svetlik@gmail.com>
2023-04-19 15:47:01 -0500

    first commit

---
+++ src/components/Dropdown.js
@@ -0,0 +1,15 @@
+
+export default function Dropdown(props){
+    return (
+
+    <form>
+    <label for="chars">Choose a character:</label>
+    <select name="chars" id="chars-dropdown">
+    <option value="volvo">Memphis</option>
+    <option value="saab">Franklin</option>
+    <option value="opel">Martha</option>
+    <option value="audi">Reginald</option>
+    <option value="audi">Ephrem</option>
+    </select>
+</form>
+)}
