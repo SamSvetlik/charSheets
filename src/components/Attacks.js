e06676d9bc33b66eff9d4d0e5e03ef3076cf14a0
SamSvetlik <sam.svetlik@gmail.com>
2023-05-11 21:31:45 -0500

    chore: styling stats and skills 2

--- src/components/Attacks.js
+++ src/components/Attacks.js
@@ -7,10 +7,12 @@ export default function Attacks(props) {
             <ul>
             {props.attacks.map((attack, index) => {
                 return (<li key={index}>
-                    <span>{attack.name}</span>
-                    <span>{attack.bonus}</span>
-                    <span>{attack.damage}</span>
-                    {attack.special ?<div>{attack.special}</div> : null }
+                    <div className="attackInfo">
+                        <span>{attack.name}</span>
+                        <span>{attack.bonus}</span>
+                        <span>{attack.damage}</span>
+                    </div>
+                    {attack.special ?<div className="attackSpecial">{attack.special}</div> : null }
                 </li>)
             })}
             </ul>
